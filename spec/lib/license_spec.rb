d25145ad0d57db71513aba2c18f4422f262c5423
Corey D. Taylor <taylorcoreyd@icloud.com>
2026-05-23 00:31:56 -0600

    feat: Added licensing pipeline

---
+++ spec/lib/license_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe 'License Compliance' do
+  it 'does not contain any unapproved gem licenses' do
+    # Runs license_finder and captures stdout/stderr
+    output = `bundle exec license_finder`
+    
+    # license_finder outputs 'All dependencies are approved' if clear
+    expect(output).to include('All dependencies are approved')
+  end
+end
