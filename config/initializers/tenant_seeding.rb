1031246ef8ae98a369439eb5340b870ccc849354
Corey D. Taylor <taylorcoreyd@icloud.com>
2026-05-22 20:42:24 -0600

    fix: Handle seeding (don't seed) when no environment variable or no domains present

--- config/initializers/tenant_seeding.rb
+++ config/initializers/tenant_seeding.rb
@@ -1,4 +1,4 @@
 Rails.application.config.to_prepare do
-  domains = ENV['DOMAINS'].split(/,\s*/)
-  TenantSeeder.seed(domains)
+  domains = ENV["DOMAINS"]&.split(/,\s*/)
+  TenantSeeder.seed(domains) if domains.present?
 end
